10ef56303f30e5ea839cf03da904491c1b155847
tmhanon <tmhanon@ucdavis.edu>
2018-10-16 18:04:08 -0700

    Loading data. Lots of it.

--- Ag_Paper_1.R
+++ Ag_Paper_1.R
@@ -8,3 +8,9 @@ library(mfx)
 options(scipen=50)
 
 # Load In Data
+cpi <- read.csv("Data/CPI.csv")
+fao_data <- read.csv("Data/FAOSTAT_data_2017.csv")
+stocks_data <- read.csv("Data/FAOSTAT_stocks_data_2017.csv")
+price_data <- read.dta13("Data/global_prices.dta")
+quant_data <- read.dta13("Data/global_quantities.dta")
+hemi_data <- read.dta13("Data/hemisphere.dta")
